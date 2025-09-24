f0caa0f5efc7695c7b23c1403db47f4beb14f377
José Miguel Trinidad R. <jm.trinidad.99@hotmail.com>
2025-09-24 11:07:39 -0400

    Config beans
    Primera parte donde tengo un ejemplo de como configurar el contenedor de depencias con un .xml, aunque tengo la opcion de hacer con notaciones pero esto es la base de todo

---
+++ class-1/src/main/java/com/coursojava/App.java
@@ -0,0 +1,18 @@
+package com.coursojava;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class  App
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        CalculatorService calculatorService=(CalculatorService) context.getBean("calculatorService");
+
+
+        System.out.println( calculatorService.helloWorld());
+    }
+}
---
+++ class-1/src/main/java/com/coursojava/CalculatorService.java
@@ -0,0 +1,13 @@
+package com.coursojava;
+
+public class CalculatorService {
+
+
+    public CalculatorService() {
+        System.out.println("Ejecutando el Constructor CalculatorService");
+    }
+
+    public String helloWorld(){
+        return  "Hello World 2 !";
+    }
+}
