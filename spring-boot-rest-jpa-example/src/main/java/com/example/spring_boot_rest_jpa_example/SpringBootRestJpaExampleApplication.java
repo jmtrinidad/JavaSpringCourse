b32a71772f049ef1d22b994d3bf2ff05f41b3ede
José Miguel Trinidad R. <jm.trinidad.99@hotmail.com>
2025-09-24 17:23:09 -0400

    Added project Spring Boot Rest with JPA

---
+++ spring-boot-rest-jpa-example/src/main/java/com/example/spring_boot_rest_jpa_example/SpringBootRestJpaExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.spring_boot_rest_jpa_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestJpaExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootRestJpaExampleApplication.class, args);
+  }
+}
---
+++ spring-boot-rest-jpa-example/src/test/java/com/example/spring_boot_rest_jpa_example/SpringBootRestJpaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_rest_jpa_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestJpaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
