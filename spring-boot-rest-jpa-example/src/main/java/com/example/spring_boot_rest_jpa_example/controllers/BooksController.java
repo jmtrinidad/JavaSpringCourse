69f05edc2478f083d5e75d74d4d81bc384096d6a
José Miguel Trinidad R. <jm.trinidad.99@hotmail.com>
2025-09-27 19:07:35 -0400

    Example GetById and post

--- spring-boot-rest-jpa-example/src/main/java/com/example/spring_boot_rest_jpa_example/controllers/BooksController.java
+++ spring-boot-rest-jpa-example/src/main/java/com/example/spring_boot_rest_jpa_example/controllers/BooksController.java
@@ -2,32 +2,69 @@ package com.example.spring_boot_rest_jpa_example.controllers;
 
 import com.example.spring_boot_rest_jpa_example.models.Book;
 import com.example.spring_boot_rest_jpa_example.repositories.BookRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-//@RequestMapping("/books")
+// @RequestMapping("/books")
 public class BooksController {
 
-    private  BookRepository bookRepository;
+  private BookRepository bookRepository;
 
-    public BooksController(BookRepository bookRepository) {
-        this.bookRepository=bookRepository;
+  public BooksController(BookRepository bookRepository) {
+    this.bookRepository = bookRepository;
+  }
+
+  @GetMapping("/api/books")
+  public List<Book> getall() {
+
+    return bookRepository.findAll();
+  }
+
+  @GetMapping("/api/books/{id}")
+  public ResponseEntity<Book> getby(@PathVariable Long id) {
+
+    Optional<Book> bookOptional = bookRepository.findById(id);
+
+    if (bookOptional.isPresent()) {
+      return ResponseEntity.ok(bookOptional.get());
     }
 
-    @GetMapping("/api/getAll")
-    public List<Book> getall(){
+    return ResponseEntity.notFound().build();
 
-        return bookRepository.findAll();
+    // Option 2
+    // return bookOptional.orElse(null);
+
+    // Option 3, PROGRAMACION FUNCIONAL
+
+    //      return bookOptional.map(ResponseEntity::ok).orElse(()->
+    // ResponseEntity.notFound().build());
+  }
+
+
+    @PostMapping("/api/books")
+    public ResponseEntity<Book> pot(@RequestBody Book book) {
+
+       var result= bookRepository.save(book);
+
+       return  ResponseEntity.ok(result);
     }
+    @PutMapping("/api/books")
+    public ResponseEntity<Book> put(@RequestBody Book book) {
 
-//    @GetMapping("/api/getby/{id}")
-//    public Book getby(Long id){
-//
-//        return bookRepository.findById(id);
-//    }
+      if(book ==null)
+          ResponseEntity.badRequest().build();
 
+      Optional<Book> optionalBook = bookRepository.findById(book.getId());
+
+        if(optionalBook.isEmpty())
+            ResponseEntity.badRequest().build();
+
+       var result= bookRepository.saveAndFlush(book);
+
+       return  ResponseEntity.ok(result);
+    }
 }
